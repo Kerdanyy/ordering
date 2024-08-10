9b4737a5365ebbdb0e099037f532ed85490f1c66
Aly El-Kerdany <kerdany@hotmail.co.uk>
2024-08-10 19:31:01 +0300

    add exception handling

---
+++ src/main/java/com/foodics/ordering/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.foodics.ordering.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseBody
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
