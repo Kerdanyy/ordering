24e7b4239e5d712be93d1d5d13e744dd4d048e5e
Aly El-Kerdany <kerdany@hotmail.co.uk>
2024-08-10 17:51:19 +0300

    Edit Order Service

--- src/main/java/com/foodics/ordering/Constants.java
+++ src/main/java/com/foodics/ordering/Constants.java
@@ -3,7 +3,6 @@ package com.foodics.ordering;
 public final class Constants {
 
     private Constants() {
-        throw new AssertionError("Cannot instantiate the constants class");
     }
 
     public static final String STOCK_COLLECTION_NAME = "stock";
--- src/main/java/com/foodics/ordering/service/OrderService.java
+++ src/main/java/com/foodics/ordering/service/OrderService.java
@@ -6,6 +6,7 @@ import com.foodics.ordering.model.Order;
 import com.foodics.ordering.model.Product;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Transaction;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,17 +22,23 @@ public class OrderService {
     @SneakyThrows
     public void addOrder(Order order) {
         HashMap<String, Ingredient> newStock = new HashMap<>();
-        Iterable<DocumentReference> currentStock = firestore.collection(Constants.STOCK_COLLECTION_NAME).listDocuments();
-        for (DocumentReference docRef : currentStock) {
-            Ingredient currentIngredientStock = docRef.get().get().toObject(Ingredient.class);
-            for (Product product : order.getProducts()) {
-                for (int i = 0; i < product.getQuantity(); i++) {
-                    adjustStockQuantity(currentIngredientStock, newStock);
+        firestore.runTransaction((Transaction transaction) -> {
+            Iterable<DocumentReference> currentStock = firestore.collection(Constants.STOCK_COLLECTION_NAME).listDocuments();
+            for (DocumentReference docRef : currentStock) {
+                Ingredient currentIngredientStock = transaction.get(docRef).get().toObject(Ingredient.class);
+                for (Product product : order.getProducts()) {
+                    if (product.getId() != 1) {
+                        throw new IllegalArgumentException("Only product with ID 1 is supported currently");
+                    }
+                    for (int i = 0; i < product.getQuantity(); i++) {
+                        adjustStockQuantity(currentIngredientStock, newStock);
+                    }
                 }
             }
-        }
-        newStock.forEach((ingredientName, ingredient) -> firestore.collection(Constants.STOCK_COLLECTION_NAME).document(ingredientName).set(ingredient));
-        firestore.collection(Constants.ORDER_COLLECTION_NAME).document().set(order);
+            newStock.forEach((ingredientName, ingredient) -> transaction.set(firestore.collection(Constants.STOCK_COLLECTION_NAME).document(ingredientName), ingredient));
+            transaction.set(firestore.collection(Constants.ORDER_COLLECTION_NAME).document(), order);
+            return null;
+        }).get();
     }
 
     private void adjustStockQuantity(Ingredient ingredient, HashMap<String, Ingredient> newStock) {
@@ -39,10 +46,11 @@ public class OrderService {
             case ("Beef") -> ingredient.setQuantity(ingredient.getQuantity() - 150);
             case ("Cheese") -> ingredient.setQuantity(ingredient.getQuantity() - 30);
             case ("Onion") -> ingredient.setQuantity(ingredient.getQuantity() - 20);
+            default -> throw new IllegalArgumentException("Unsupported ingredient: " + ingredient.getName());
         }
         newStock.put(ingredient.getName(), ingredient);
         if (ingredient.getQuantity() < 0) {
-            throw new AssertionError("Order cannot be completed as ingredient: " + ingredient.getName() + "stock is not enough");
+            throw new IllegalArgumentException("Order cannot be completed as ingredient: " + ingredient.getName() + "stock is not enough");
         }
     }
 }
